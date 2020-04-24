b396939cd5f140d49036a420d4c6c889ed76c898
Asatogenki <asa.326.gen@gmail.com>
2020-04-25 01:58:18 +0900

    基本機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    # orderメソッドで降順:descか昇順:asc指定ができる
     @posts = Post.all.order(created: :desc)
   end
 
@@ -8,6 +9,7 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
@@ -17,14 +19,33 @@ class PostsController < ApplicationController
   def update
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
-    @post.save
-    redirect_to("/posts/index")
+    if @post.save
+      redirect_to("/posts/index")
+      # flash[:notice]でフラッシュメッセージ設定HTML側にも必要
+      flash[:notice] = "投稿を編集しました"
+    else
+      # editアクション経由ではなくそのままposts/editを表示できる
+      render("posts/edit")
+    end
   end
 
 
   def create
     @post = Post.new(content: params[:content])
-    @post.save
-    redirect_to("/posts/index")
+    if @post.save
+      redirect_to("/posts/index")
+      flash[:notice] = "投稿しました"
+    else
+      render("posts/new")
+    end
   end
+
+  def destroy
+    # find_byメソッドで()内の条件に合致するデータを習得することができる
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+    flash[:notice] = "削除しました"
+  end
+
 end
